1ca57533a7b444de23c38efc3369f9c54652b3f8
juris <juris@zee.lv>
2018-01-30 17:42:13 +0200

    bump pypy to 5.10

--- install-pypy.sh
+++ install-pypy.sh
@@ -4,14 +4,14 @@
 #
 
 PYPY_PATH="/opt/pypy"
-PYPY_VERSION="5.6"
+PYPY_VERSION="5.10.0"
 
 if [ "$(whoami)" != "core" ]; then
   echo 'Please run script as "core" user'
   exit 1
 fi
 
-# check if PyPy is already there
+# Check if PyPy is already there
 if $(${PYPY_PATH}/bin/pypy --version &> /dev/null); then
   echo "Looks like you've got pypy in place! So long!"
   exit 0
@@ -19,7 +19,7 @@ fi
 
 sudo mkdir -p $PYPY_PATH /opt/bin
 sudo chown core:core $PYPY_PATH
-curl -Lo $PYPY_PATH/pypy.tar.bz2 https://bitbucket.org/squeaky/portable-pypy/downloads/pypy-${PYPY_VERSION}-linux_x86_64-portable.tar.bz2
+curl -Lso $PYPY_PATH/pypy.tar.bz2 https://bitbucket.org/squeaky/portable-pypy/downloads/pypy-${PYPY_VERSION}-linux_x86_64-portable.tar.bz2
 tar jxf $PYPY_PATH/pypy.tar.bz2 -C $PYPY_PATH --strip-components 1
 rm $PYPY_PATH/pypy.tar.bz2
 sudo ln -s $PYPY_PATH/bin/pypy /opt/bin/python
